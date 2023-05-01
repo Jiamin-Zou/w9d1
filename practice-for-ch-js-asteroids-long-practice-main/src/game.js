521b2b32eef5c36ef6337d2b90e189e121aa6f60
Jiamin-Zou <jiaminz622@gmail.com>
2023-05-01 15:57:47 -0400

    Added Game class, #constructor, #addAsteroids, #draw, #moveObjects, #randomPosition

---
+++ practice-for-ch-js-asteroids-long-practice-main/src/game.js
@@ -0,0 +1,44 @@
+import Asteroid from "./asteroid.js";
+
+class Game {
+    static DIM_X = 1500;
+    static DIM_Y = 900;
+    static NUM_ASTEROIDS = 10;
+
+    constructor(){
+        this.addAsteroids()
+    }
+
+    addAsteroids() {
+        this.asteroids = [];
+        while (this.asteroids.length < Game.NUM_ASTEROIDS) {
+            const position = this.randomPosition()
+        const ast = new Asteroid({pos: position});
+        this.asteroids.push(ast)
+        }
+    }
+
+    draw(ctx) {
+        ctx.clearRect(0, 0, Game.DIM_X, Game.DIM_Y);
+        this.asteroids.forEach( asteroid => {
+            asteroid.draw(ctx);
+        })
+    }
+    
+    moveObjects() {
+        this.asteroids.forEach( asteroid => {
+            asteroid.move();
+        })
+
+    }
+  
+    randomPosition() {
+      const randX = Math.floor(Math.random() * Game.DIM_X);
+      const randY = Math.floor(Math.random() * Game.DIM_Y);
+      const randPos = [randX, randY];
+
+      return randPos;
+    }
+  }
+
+  export default Game;
--- practice-for-ch-js-asteroids-long-practice-main/src/index.js
+++ practice-for-ch-js-asteroids-long-practice-main/src/index.js
@@ -2,14 +2,13 @@
 
 // window.MovingObject = MovingObject;
 
-import Asteroid from "./asteroid.js";
+import Game from "./game.js";
 
 
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 
-const mo = new Asteroid({ pos: [30, 30] });
-
-  mo.draw(ctx);
-//   setInterval(() => {mo.move(); mo.draw(ctx)},20)
-
+const ga = new Game();
+ga.draw(ctx);
+ga.moveObjects();
+setInterval(() => { ga.draw(ctx); ga.moveObjects();},50) 
